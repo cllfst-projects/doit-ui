f24cba3e24be63ae0f178c9c9d176eb4b0314d85
laribihoussem <laribihoussem1998@gmail.com>
2020-12-22 11:45:06 +0100

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [
-  { path:'login', component: LoginComponent }
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
