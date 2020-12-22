d283eba713cdb888ea1e63a704b84530633659dc
Eyabouka <eyaboukadhaba@gmail.com>
2020-12-22 12:30:37 +0100

    footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
  
-  { path: 'app' , component: FooterComponent },
 
   
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
