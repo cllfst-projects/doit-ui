a6dcc60be3600453156555626c615a66f595d8f0
Nasri Haithem <mai5haithem@gmail.com>
2020-12-27 21:45:10 +0100

    Fixing Conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
-=======
-import { HomeComponent } from './home/home.component';
->>>>>>> develop
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HeaderComponent,
-    HomeComponent
-=======
-    HomeComponent
->>>>>>> develop
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
