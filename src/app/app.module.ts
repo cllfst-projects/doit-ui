c51391a2ca10208d7caa0e6035cf1cf7a15881a3
Eyabouka <eyaboukadhaba@gmail.com>
2020-12-28 13:09:49 +0100

    changing HomeComponent path after moving it in Components component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
