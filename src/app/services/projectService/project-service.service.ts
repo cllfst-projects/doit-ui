e8fd2fa2b829ac29af3c23d5c342ce7e2f64ab75
Nasri Haithem <mai5haithem@gmail.com>
2021-03-14 19:40:02 +0100

    Adding a folder that contains services, adding a service that provides user's projects to the sidepanel component

---
+++ src/app/services/projectService/project-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {PROJECTS} from '../../mockData/projects'
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectServiceService {
+
+  constructor() { }
+  getProjects() {
+    const projects = PROJECTS;
+    return projects
+  }
+}
