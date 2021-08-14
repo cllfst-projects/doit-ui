451ba6f46735f301ea1f63a415fba2c00f9c08e8
NasriHaithem <mai5haithem@gmail.com>
2021-08-14 18:35:31 +0100

    adding an input to add new items in each card

---
+++ src/app/mockData/cardItems.js
@@ -0,0 +1,31 @@
+exports.ITEMS = [{
+        cardName: "To Do",
+        cardItems: [
+            "Projeyt JAVA ALGO",
+            "PFA",
+            "PAUD",
+            "OC",
+            "CyberSecurity",
+            "Stochastique",
+            "IF4 subjects"
+        ]
+    },
+    {
+        cardName: "Doing",
+        cardItems: [
+            "DOIT Project",
+            "Soc",
+            "Adminis. Réseau",
+            "EVE ng"
+        ]
+    },
+    {
+        cardName: "Done",
+        cardItems: [
+            'Get to work',
+            'Pick up groceries',
+            'Go home',
+            'Fall asleep'
+        ]
+    }
+]
